c6e5f38d578f4a53c1971c3c77e873106f796d89
xdcarcube <68501767+xdcarcube@users.noreply.github.com>
2020-08-10 20:28:49 +0200

    Add files via upload

---
+++ TEST.py
@@ -0,0 +1,44 @@
+#Import`s
+import time
+# import random comes later on (isnt implemented yet ;-))
+import random
+#Language Select (Work in Progress)
+Laguage_Select = input("Select your Language! Wähle deine Sprache! (DE/EN) ")
+if Laguage_Select == "DE" or "De" or "de":
+    print("Hallo, dies ist mein erstes kleines Projekt, Viel Spaß =]")
+elif Laguage_Select == "EN" or "En" or "en":
+    print("Hi, This is my First Project im currently working on! ")
+time.sleep(2)
+#Tutorial
+def Help():
+    print("You can use *Tutorial_DE* or *Tutorial_EN*")
+def Tutorial():
+    print("Schreibe *Links* um einen Schritt nach Links zu gehen, *Rechts* um nach rechts zu gehen, *Geradeaus* um Geradeaus zu gehen und *Unten* um nach Unten zu gehen ")
+#Vor dem Start
+print("Es geht los in! ")
+time.sleep(1)
+print("5")
+time.sleep(1)
+print("4")
+time.sleep(1)
+print("3")
+time.sleep(1)
+print("2")
+time.sleep(1)
+print("1")
+time.sleep(1)
+print("Los gehts =) ")
+time.sleep(2)
+#Start
+Playername = input("Please Type your Name: ")
+if Playername == "Felix":
+    print("Just Wait a few... ")
+else:
+    print("Oops im sorry for what happened =[ , please restart and type *Felix* ")
+    END
+time.sleep(3)
+#bruh XD
+print("Es war einmal ein Kleiner Zauberer der etwas Ganz tun werden wollte... BLA BLA BLA DEN REST KÖNNT IHR EUCH SELBST DENKEN")
+time.sleep(4)
+print("XD")
+#Start des eigentlichen Spiels =)
