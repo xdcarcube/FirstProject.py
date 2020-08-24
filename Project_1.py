2c109628c4b925ea7b1dc69f0aa8290a48933c72
xdcarcube <68501767+xdcarcube@users.noreply.github.com>
2020-08-24 13:43:13 +0200

    Update Project_1.py

--- Project_1.py
+++ Project_1.py
@@ -1,33 +1,40 @@
+# Copyright (c) 2018 Copyright Holder All Rights Reserved.
 import time
 # import random comes later on
 from random import randint
 #Later(not yet)
 import tkinter
 
-import sys
-# Funktions
+# Funktions\Classes
 
-class PlayerOne:
+class Ex(Exception):
+    pass
+
+class PlayerOne():
+    def __init__(self):
+        pass
     MoneyListvar = [0]
     inventoryvar = ["/100xp"]
     def Inventory(self):
-        return inventoryvar
+        return inventoryvar[0]
     def money(self):
-        return MoneyListvar
+        return MoneyListvar[0]
 
 class Tutorials():
     def TutorialDE(self):
-        tutDE = print("Schreibe *Links* um einen Schritt nach Links zu gehen, *Rechts* um nach rechts zu gehen, \
-                        *Geradeaus* um Geradeaus zu gehen und *Unten* um nach Unten zu gehen.")
+        tutDE = print(str("Schreibe *Links* um einen Schritt nach Links zu gehen, *Rechts* um nach rechts zu gehen, \
+                        *Geradeaus* um Geradeaus zu gehen und *Unten* um nach Unten zu gehen."))
         return tutDE
 
     def TutorialEN(self):
-        tutEN = print("Type (Forward) to go Forward, Type (Right) to go Right, (Left) to get to dat left and (Down) to go Backwards.")
+        tutEN = print(str("Type (Forward) to go Forward, Type (Right) to go Right, (Left) to go left and (Down) to go Backwards."))
         return tutEN
 
     def Help(self):
-        hlp = print("You can Type: TutorialDE/TutorialEN/money/inventory")
+        hlp = print(str("You can always type quit to quit()/TutorialEN/money/inventory/TutorialDE <--(I´d would´nt recommend typing this if you dont have knowlegde besides the German Language)"))
         return hlp
+
+Help = ["TutorialEN", "money" "inventory"] # (if the class is not working ;-;)
 
 
 # CHANGES ON "def Help()" AND "def Tutorial()" DESIRED!!!
@@ -37,7 +44,7 @@ class Tutorials():
 # Lists
 
 
-# Some Strings i need
+# Some Strings
 Lets_Go = "Lets Go! "
 
 greeting = "Hey, "
@@ -48,114 +55,98 @@ first_story_DE = "Es war einmal ein Kleiner Zwerg, der wie sein Vater sein Wollt
 first_story_EN = 'Once Upon a time a little Witcher wanted to be like his Dad. Later on he travelled around the World ' \
                  'and gained Respect and Coins '
 
-this_is_for_going_event_1 = 0
+this_is_for_going_event_1 = 0 # isnt used lol
 
 forvar = " of 5 actions"
 
 # LANG SELECT
-Language_Select = input("Select your Language! Wähle deine Sprache! (DE/EN) ")
+Language_Select = input(str("Select your Language! Wähle deine Sprache! (Deutsch ist bei weitem nicht Vollständig!) (DE/EN) \n"))
 if Language_Select == "DE":
-    print("Hallo, dies ist mein erstes kleines Projekt, Viel Spaß =]")
+    print(str("Hallo, dies ist mein erstes kleines Projekt, Viel Spaß =]"))
 elif Language_Select == "EN":
-    print("Hi, This is my First Project im currently working on! ")
+    print(str("Hi, This is my First Project im currently working on! "))
 elif Language_Select != "EN" or "DE":
-    print("What are you doing?! Restart...Stupid ")
-    SystemExit
+    print(str("What are you doing?! Restart..."))
+    quit()
 time.sleep(2)
 
 
 #start?
 if Language_Select == "EN":
-    startgameEN = input("Do you want to Start the Game? Y/N ")
+    startgameEN = input(str("Do you want to Start the Game? Y/N \n"))
 elif Language_Select == "DE":
-    startgameDE = input("Willst du das Spiel starten? J/N ")
-
-
-
-
+    startgameDE = input(str("Willst du das Spiel starten? J/N \n"))
 
 
 # Before the start
 for i in range(6, 0):
-    print(i)
+    print(int(i))
     time.sleep(1)
 
 
 # playername
 if Language_Select == "EN":
-    Playername_1 = input("Please Type your Name: ")
-    print(greeting + Playername_1)
+    Playername_1 = str(input("Please Type your Name: \n"))
 elif Language_Select == "DE":
-    Playername_2 = input("Bitte gebe deinen Namen ein: ")
-time.sleep(3)
+    Playername_1 = str(input("Bitte gebe deinen Namen ein: \n"))
+else:
+    print(str("What are you doing?! Restart..."))
+time.sleep(1.5)
 
 
 # (Placeholder)
-print("Loading Data ")
-for herunterzählen in range(1, 7):
-    print("...")
-    time.sleep(1)
-print("Value Error in Project_1.py <module> print() is not defined! ")
-time.sleep(3.5)
-if Language_Select == "DE":
-    print("Haha, Reingelegt " + Lets_Go)
-if Language_Select == "EN":
-    print("Haha Just Kidding, " + Lets_Go)
-time.sleep(1)
 
 
-# "Story" Part
-if Language_Select == "EN":
-    print(first_story_EN)
-elif Language_Select == "DE":
-    print(first_story_DE)
-time.sleep(4)
-if Language_Select == "DE":
-    print(greeting + Playername_2 + "!")
-elif Language_Select == "EN":
-    print(greeting + Playername_1 + "!")
+# "Story" Part (no more)
+time.sleep(2.5)
+if Language_Select == "DE" or "EN":
+    print(str(greeting + Playername_1 + "!"))
 
 time.sleep(1.5)
 if Language_Select == "EN":
-    print("This is the beginning of a big Story of " + Playername_1)
-if Language_Select == "DE":
-    print("Das ist der Anfang Einer Geschichte von " + Playername_2)
+    print(str("This is the beginning of a big Story of " + Playername_1))
+elif Language_Select == "DE":
+    print(str("Das ist der Anfang Einer Geschichte von " + Playername_1))
 time.sleep(0.5)
-time.sleep(1)
 
-# So i will add the german Language Later on, as i did before (Firstly: EN)
-print("Now...Roll the dice to choose your Destiny! ")
+# So, i will add the german Language Later on, as i did before (Firstly: EN)
+print(str("The God of Dices has chosen you ...(*°O°)ノ"))
 time.sleep(2)
 # Dice Randint
 Dice_Roll = randint(1, 6)
-print(Dice_Roll)
 #            1=1000Coins 2=800 3=600 4=400 5=200 6=100
-if Dice_Roll == "1":
+if Dice_Roll == 1:
     instanz1 = PlayerOne
     listDEL1 = instanz1.MoneyListvar.remove(0)
-    listADD1 = instanz1.money.append(int(1000))
-    print("Nice! You Hit the Max  ")
-if Dice_Roll == "2":
+    listADD1 = instanz1.MoneyListvar.append(int(1000))
+    print(str("Nice! You Hit the Max  "))
+elif Dice_Roll == 2:
     instanz2 = PlayerOne
     listDEL2 = instanz2.MoneyListvar.remove(0)
-    listADD2 = instanz2.money.append(int(800))
-if Dice_Roll == "3":
+    listADD2 = instanz2.MoneyListvar.append(int(800))
+    print(str("Two? Thats good!"))
+elif Dice_Roll == 3:
     instanz3 = PlayerOne
     listDEL3 = instanz3.MoneyListvar.remove(0)
-    listADD3 = instanz3.money.append(int(600))
-if Dice_Roll == "4":
+    listADD3 = instanz3.MoneyListvar.append(int(600))
+    print(str("Three is viable"))
+elif Dice_Roll == 4:
     instanz4 = PlayerOne
-    listDEL4 = instanz4.MoneyList.remove(0)
-    listADD4 = instanz4.money.append(int(400))
-if Dice_Roll == "5":
+    listDEL4 = instanz4.MoneyListvar.remove(0)
+    listADD4 = instanz4.MoneyListvar.append(int(400))
+    print(str("ough four"))
+elif Dice_Roll == 5:
     instanz5 = PlayerOne
-    listDEL5 = instanz5.MoneyList.remove(0)
-    listADD5 = instanz5.money.append(int(200))
-if Dice_Roll == "6":
+    listDEL5 = instanz5.MoneyListvar.remove(0)
+    listADD5 = instanz5.MoneyListvar.append(int(200))
+    print(str("ough five"))
+elif Dice_Roll == 6:
     instanz6 = PlayerOne
-    listDEL6 = instanz6.MoneyList.remove(0)
-    listADD6 = instanz6.money.append(int(100))
-    print("Haha you hit the Worst x_x ")
+    listDEL6 = instanz6.MoneyListvar.remove(0)
+    listADD6 = instanz6.MoneyListvar.append(int(100))
+    print(str("Haha you hit the Worst x_x "))
+print(int(Dice_Roll))
+#add XP
 instanz7 = PlayerOne
 listADD7 = instanz7.inventoryvar.insert(0, "10")
 time.sleep(2)
@@ -164,20 +155,53 @@ time.sleep(2)
 x = 0
 # left
 y = 0
-# up
-up = 0
+# forward
+forward = 0
 # down
 down = 0
 
-print("Type (Help) ")
-time.sleep(1.5)
-for going_event_1 in range(5):
-        going_input = input("What do you wanna do next? ")
-        print(this_is_for_going_event_1) + (forvar)
-        this_is_for_going_event_1 +1
-        if going_input == "Help":
-            instanz8 = Tutorials
-            print(instanz8.TutorialEN)
-        elif going_input == "Rechts" or "Right":
-            x + 1
-            print("Ok! ")
+
+
+
+# MAIN CODE PART!!!
+if __name__ == "__main__":
+    while True:
+        for i in range(1):
+            going_input1 = str(input("The adventure just began, type (Help) to get advice! \n"))
+        for i in range(3):
+            going_input2 = str(input("\n"))
+            if going_input1 == "Help":
+                instanz8 = Tutorials
+                instanz8Acces = instanz8.Help
+                print(str(instanz8Acces))
+            elif going_input2 == "TutorialEN":
+                instanz9 = Tutorials
+                instanz9Acces = instanz9.TutorialEN
+                print(str(instanz9Acces))
+            elif going_input2 == "money":
+                instanz10 = PlayerOne
+                instanz10Acces = PlayerOne.MoneyListvar
+                print(str(instanz10Acces))
+            elif going_input2 == "inventory":
+                instanz11 = PlayerOne
+                instanz11Acces = PlayerOne.inventoryvar
+                print(str(instanz11Acces))
+            elif going_input2 == "Rechts" or "Right":
+                x = x + 1
+                print(str("rightok"))
+                print(int(x))
+            elif going_input2 == "Links" or "Left":
+                y = y + 1
+                print(str("leftok"))
+                print(int(y))
+            elif going_input2 == "Vorwärts" or "Forward":
+                forward = forward + 1
+                print(str("forwardok"))
+                print(int(forward))
+            elif going_input2 == "Runter" or "down":
+                down = down + 1
+                print(str("downok"))
+                print(int(down))
+            else:
+                print(str("Undefined Input!"))
+print(str("This is end, has it worked well?"))
